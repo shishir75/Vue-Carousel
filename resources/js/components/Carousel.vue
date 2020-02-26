47d0bf24d8e546c1c47a5d3e3f213bcb2af4b41f
shishir75 <shishir.srdr16@gmail.com>
2020-02-26 15:35:00 +0600

    flickity configure

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Carousel Component</div>
+
+                    <div class="card-body">
+                        <div data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true }'>
+                            <slot></slot>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import Flickity from 'flickity';
+    import 'flickity/dist/flickity.min.css';
+
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        components: [Flickity]
+    }
+</script>
